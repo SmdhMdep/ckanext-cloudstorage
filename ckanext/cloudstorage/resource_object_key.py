d54c2c73a26ec21e1919374512b3a0a3aebbe295
Ayman Izzeldin <ayman.izzeldin@stfc.ac.uk>
2023-08-14 03:13:08 +0100

    model resource object key with implicit naming convention
    parse resource object key into rich objects
    create resource object key representation from package and resource

---
+++ ckanext/cloudstorage/resource_object_key.py
@@ -0,0 +1,183 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+import logging
+from typing import Optional
+from abc import abstractclassmethod, ABC
+
+
+logger = logging.getLogger(__name__)
+_FACTORIES = []
+
+
+class ResourceObjectKeyType(Enum):
+    UPLOAD = 'upload'
+    STREAMING = 'streaming'
+
+
+@dataclass
+class ResourceObjectKey(ABC):
+    raw: str
+    """The raw object key name."""
+
+    version: int
+    """The version of this key format. Must be defined as a static attribute on the class."""
+
+    organization_name: str
+    """The id of the organization for this resource."""
+
+    package_name: str
+    """The id of the package for this resource"""
+
+    name: str
+    """The name of the resource. For uploaded data, this matches the filename. For streamed data, 
+    this is the folder name (prefix part) right after the package name.
+    """
+
+    type: ResourceObjectKeyType
+    """The key type. Determines the underlying resource type this key references."""
+
+    filename: str
+    """Matches the object name in S3"""
+
+    ingestion_datetime: Optional[datetime]
+    """For streamed data, the date this data was ingested. None for uploaded data."""
+
+    @classmethod
+    def _factories(cls):
+        # can't use `__subclasses__` directly here as it doesn't account for deeper hierarchies.
+        return sorted(_FACTORIES, key=lambda f: f.version, reverse=True)
+
+    @classmethod
+    def from_raw_key(cls, key: str) -> 'ResourceObjectKey':
+        for factory in cls._factories():
+            instance = factory.try_parse(key)
+            if instance is not None:
+                return instance
+        raise ValueError(f'Unsupported key format: {key}')
+
+    @classmethod
+    def from_resource(cls, package: dict, resource: dict) -> 'ResourceObjectKey':
+        key = resource.get('cloud_storage_key')
+        if key is not None:
+            return cls.from_raw_key(key)
+        for factory in cls._factories():
+            instance = factory.try_create(package, resource)
+            if instance is not None:
+                return instance
+        raise ValueError(f'Cannot create key for resource: {resource.get("id")}')
+
+    @abstractclassmethod
+    def try_parse(cls, key: str) -> 'ResourceObjectKey':
+        pass
+
+    @abstractclassmethod
+    def try_create(cls, package: dict, resource: dict) -> 'ResourceObjectKey':
+        pass
+
+    def __init_subclass__(cls) -> None:
+        _FACTORIES.append(cls)
+        return super().__init_subclass__()
+
+
+class _ResourceObjectKeyV0(ResourceObjectKey):
+    """
+    Version 1 of the resource object key format.
+
+    Upload resource format:
+      <org_name> / <package_name> / <resource_name = resource_filename>
+    Streaming resource format:
+      <org_name> / <package_name> / <resource_name> / ... / <resource_filename>
+
+    Distinguishing between the two types is solely based on the path length
+    """
+    version = 0
+
+    @classmethod
+    def try_parse(cls, key):
+        return _parse_from_path_v0_v1(cls, key, key, cls.version)
+
+    @classmethod
+    def try_create(cls, package, resource):
+        organization_name = package['organization']['name']
+        package_name = package['name']
+        name = resource.get('name')
+        return cls.try_parse(f'{organization_name}/{package_name}/{name}')
+
+
+class _ResourceObjectKeyV1(ResourceObjectKey):
+    """
+    Version 1 of the resource object key format. This is similar to version 0 but
+    encodes the version number in the key, and enforces the file name format for streamed data.
+
+    Upload resource format:
+      <version> / <org_name> / <package_name> / <resource_name = resource_filename>
+    Streaming resource format:
+      <version> / <org_name> / <package_name> / <resource_name> / ... / <resource_filename>
+
+    Distinguishing between the two types is solely based on the path length
+    """
+    version = 1
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.type == ResourceObjectKeyType.STREAMING and not self.ingestion_datetime:
+            raise ValueError("expected ingestion time to be included in the filename for streamed data")
+
+    @classmethod
+    def try_parse(cls, key):
+        version, _, path = key.partition('/')
+        if int(version) == cls.version:
+            return _parse_from_path_v0_v1(cls, path, key, cls.version)
+        return None
+
+    @classmethod
+    def try_create(cls, package: dict, resource: dict):
+        org_name = package['organization']['name']
+        pkg_name = package['name']
+        name = resource.get('name')
+        return cls.try_parse(f'{cls.version}/{org_name}/{pkg_name}/{name}')
+
+
+def _parse_from_path_v0_v1(cls, path: str, raw: str, version: int) -> ResourceObjectKey:
+    try:
+        organization_name, package_name, *resource_path = path.split("/")
+        if not resource_path:
+            raise ValueError("key does not match the expected schema")
+    except ValueError:
+        raise ValueError("key does not match the expected schema")
+
+    name = resource_path[0]
+    filename = (
+        name if type == ResourceObjectKeyType.UPLOAD
+        else resource_path[-1]
+    )
+    ingestion_datetime = (
+        _try_parse_streaming_datetime(filename)
+        if type == ResourceObjectKeyType.STREAMING
+        else None
+    )
+
+    return cls(
+        raw=raw,
+        version=cls.version,
+        organization_name=organization_name,
+        package_name=package_name,
+        name=name, 
+        type=(
+            ResourceObjectKeyType.STREAMING if len(resource_path) > 1
+            else ResourceObjectKeyType.UPLOAD
+        ),
+        filename=filename,
+        ingestion_datetime=ingestion_datetime,
+    )
+
+def _try_parse_streaming_datetime(name) -> Optional[datetime]:
+    # example name: PUT-S3-Qj0zi-3-2023-06-26-18-42-52-3d8d51f5-0fc4-3a21-8d2e-ff614b8e9a30
+    # extract the datetime from the name, for the example this results in
+    # ingestion_datetime = ['2023', '06', '26', '18', '42', '52']
+    try:
+        ingestion_datetime = name.split('-', maxsplit=4)[-1].split('-', maxsplit=6)[:-1]
+        return datetime(*map(int, ingestion_datetime))
+    except:
+        return None
